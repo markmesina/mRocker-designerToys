bcfd566012d5b9ab75a28ae871c92eb38da87556
ceress <markmesina69@gmail.com>
2020-07-02 15:32:35 -0700

    init mongoDB

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import User from '../Model/userModel';
+
+const router = express.Router();
+
+router.get('/createadmin', async (req, res) => {
+    try {
+        const user = new User({
+            name: 'Mark',
+            email: 'markmesina69@gmail.com',
+            password: '1234',
+            isAdmin: true
+        });
+        const newUser = await user.save();
+        res.send(user);
+        
+    } catch (error) {
+        res.send({ msg: error.message });
+        
+    }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
 import express from 'express';
-import data from './data'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import data from './data';
+import config from './config';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+
+app.use('/api/users', userRoute);
 
 app.get('/api/products', (req, res) => {
     res.send(data.products)
